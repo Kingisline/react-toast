0d77d9bdca753ff60dc82f37c3d7db4c019d94ba
Tobi <callmeasgooduser@gmail.com>
2023-12-14 06:03:43 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ToastAndroid, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const showToast = () => {
+    console.log('toast clicked');
+    ToastAndroid.show(
+      "You clicked this toast!",
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello!</Text>
+      <TouchableOpacity style={styles.button} onPress={()=> showToast()}>
+        <Text>Click me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text:{
+    color: "#304ffe",
+    fontSize: 20,
+    padding: 20
+  },
+
+  button: {
+    borderRadius: 10,
+    backgroundColor: "#29b6f6",
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
